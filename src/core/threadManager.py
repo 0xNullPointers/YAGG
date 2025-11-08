2430ebbc480607df2c3c5e55b7406f2aae57053d
0xBadCod3 <218042368+0xBadCod3@users.noreply.github.com>
2025-11-08 20:58:22 +0530

    Enhance thread management with mutex protection

--- src/core/threadManager.py
+++ src/core/threadManager.py
@@ -1,4 +1,4 @@
-from PySide6.QtCore import QThread, QObject, Signal, Slot
+from PySide6.QtCore import QThread, QObject, Signal, Slot, QMutex
 
 class WorkerSignals(QObject):
     finished = Signal()
@@ -26,6 +26,17 @@ class Worker(QObject):
 class ThreadManager:
     def __init__(self):
         self.threads = []
+        self.mutex = QMutex()
+
+    def _safe_remove_thread(self, thread, worker):
+        self.mutex.lock()
+        try:
+            # Check if thread still exists in the list before removing
+            thread_tuple = (thread, worker)
+            if thread_tuple in self.threads:
+                self.threads.remove(thread_tuple)
+        finally:
+            self.mutex.unlock()
 
     def run_function(self, function, *args, **kwargs):
         # Create thread and worker
@@ -38,20 +49,38 @@ class ThreadManager:
         worker.signals.finished.connect(thread.quit)
         worker.signals.finished.connect(worker.deleteLater)
         thread.finished.connect(thread.deleteLater)
-        thread.finished.connect(lambda: self.threads.remove((thread, worker)))
+        
+        # Use a safe remove method if thread is already removed
+        thread.finished.connect(lambda t=thread, w=worker: self._safe_remove_thread(t, w))
 
-        # Store thread reference
-        self.threads.append((thread, worker))
+        # Store thread reference with mutex protection
+        self.mutex.lock()
+        try:
+            self.threads.append((thread, worker))
+        finally:
+            self.mutex.unlock()
 
         # Start thread
         thread.start()
         return worker.signals
 
     def cleanup(self):
-        # Only cleanup threads that are still running
-        for thread, _ in self.threads[:]:
+        self.mutex.lock()
+        try:
+            # Create a copy of the threads list to iterate over
+            threads_copy = self.threads[:]
+        finally:
+            self.mutex.unlock()
+        
+        # Quit all running threads
+        for thread, _ in threads_copy:
             if thread.isRunning():
                 thread.quit()
                 thread.wait(1000)
         
-        self.threads.clear()
+        # Clear the list with mutex protection
+        self.mutex.lock()
+        try:
+            self.threads.clear()
+        finally:
+            self.mutex.unlock()
